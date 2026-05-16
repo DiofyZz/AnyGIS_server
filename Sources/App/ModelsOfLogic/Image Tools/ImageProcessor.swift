cfe491de6548244804813450ef61e484acd3cf2f
DiofyZz <136701919+DiofyZz@users.noreply.github.com>
2026-05-17 00:50:22 +0500

    Update ImageProcessor.swift

--- Sources/App/ModelsOfLogic/Image Tools/ImageProcessor.swift
+++ Sources/App/ModelsOfLogic/Image Tools/ImageProcessor.swift
@@ -11,7 +11,7 @@ class ImageProcessor {
     
     // My DigitalOcean droplet with Image processing service
     //let host = "http://localhost:5000/"
-    let host = "http://mapshoter-container:5000/"
+    let host = "http://mapshoter-container:3000/"
     
     
     func attachRow(tilesUrl: [String], req: Request) throws -> Future<Response> {
